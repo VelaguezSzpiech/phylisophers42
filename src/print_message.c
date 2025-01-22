3b980ff7ff38296683da1b9e707ea566ffb72331
Velaguez <Velaguez1@gmail.com>
2025-01-22 13:50:36 +0100

    comments

--- src/print_message.c
+++ src/print_message.c
@@ -1,14 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   print_message.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/09 15:24:04 by vszpiech          #+#    #+#             */
-/*   Updated: 2025/01/19 15:41:37 by marvin           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
 #include "philosopher.h"
 
@@ -34,7 +23,6 @@ void    print_message(t_philosopher *philosopher, int message)
     int             should_print;
 
     table = philosopher->table;
-    // Check state before doing anything else
     pthread_mutex_lock(&table->check_lock);
     should_print = (!table->dead && !table->is_full) || (message == MESSAGE_DEAD);
     if (!should_print)
@@ -43,7 +31,6 @@ void    print_message(t_philosopher *philosopher, int message)
         return;
     }
     current_time = get_time() - table->start_time;
-    // If this is a death message, ensure no other messages can be printed after
     if (message == MESSAGE_DEAD)
         table->dead = 1;
     pthread_mutex_unlock(&table->check_lock);
